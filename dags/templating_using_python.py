839eda984599519539cadcfe84ceeef47a2ba70f
MutwiriFrank <mutwirifranco@gmail.com>
2023-06-07 18:44:22 +0300

    templating with python2

--- dags/templating_using_python.py
+++ dags/templating_using_python.py
@@ -2,22 +2,29 @@ from urllib import request
 import airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+import logging
+
+logger = logging.getLogger(__name__)
 
 dag = DAG(
     dag_id="stocksense",
-    start_date=airflow.utils.dates.days_ago(1),
+    start_date=airflow.utils.dates.days_ago(2),
     schedule_interval="@hourly"
 )
 
 def _get_data(logical_date):
-    year, month, day, hour, *_ = logical_date.timetuple()
+    # logger.info(f"The execution date of this flow is {ds.month}")
+    year = logical_date.year
+    month = logical_date.month
+    day = logical_date.day
+    hour = logical_date.hour
     url = (
         "https://dumps.wikimedia.org/other/pageviews/"
         f"{year}/{year}-{month:0>2}/"
         f"pageviews-{year}{month:0>2}{day:0>2}-{hour:0>2}0000.gz"
     )
-    output_path = "/tmp/wikipageviews.gz"
-    request.urlretrieve(url, output_path)
+    output_path = "../tmp/wikipageviews.gz"
+    # request.urlretrieve(url, output_path)
 get_data = PythonOperator(
     task_id="get_data",
     python_callable=_get_data,
